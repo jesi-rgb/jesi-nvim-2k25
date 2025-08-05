2132208df4b9010148491ed4ba1599f1916704cc
Jesús Enrique Rascón <blograso@gmail.com>
2025-08-05 14:31:08 +0200

    remove neotree codeium and avante, supermaven autocomplete ctrl y

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,7 +1,11 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup {}
+    require('supermaven-nvim').setup {
+      keymaps = {
+        accept_suggestion = '<C-y>',
+      },
+    }
   end,
 
   dependencies = {
